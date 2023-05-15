cd87c6261ea02a8db2156f5eae068d7c15de02a1
1xfaygo <113103007+1xfaygo@users.noreply.github.com>
2023-05-15 10:16:17 +0200

    15-5-23

---
+++ opdrachten/BE1_02.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$tafelvn6 = 6;
+for ($x = 0; $x <= 10; $x++) {
+    $totaal = $tafelvn6*$x;
+  echo "The number is:" .$totaal. "<br>";
+}
+?>  
+
+</body>
+</html>
---
+++ test/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML Formulier</title>
+</head>
+<body>
+
+	<form action="actie.php" method="post">
+		<label for="name">Naam:</label>
+		<input type="text" id="name" name="name"><br><br>
+		
+		<label for="age">Leeftijd:</label>
+		<input type="number" id="age" name="age"><br><br>
+		
+		<input type="submit" value="Verzenden">
+	</form>
+
+</body>
+</html>
